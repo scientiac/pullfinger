1761df663d71c631c0b98dc96fef28d3f6027f68
Payre <visionpeer@gmail.com>
2023-09-05 00:38:49 +0545

    default windows size

--- PullFinger.cpp
+++ PullFinger.cpp
@@ -140,7 +140,7 @@ public:
 
     // Configure window
     gtk_window_set_title(GTK_WINDOW(window), "Pullfinger");
-    gtk_window_set_default_size(GTK_WINDOW(window), 853, 480);
+    gtk_window_set_default_size(GTK_WINDOW(window), 700, 480);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
 
